8894647489050e917a1cdcb28f374f7f0f468ce6
Sanghyun Byun <sanghyub@uci.edu>
2023-06-08 01:04:33 -0700

    dataset changes

--- model.py
+++ model.py
@@ -3,7 +3,8 @@ import tensorflow as tf
 
 def generate_model():
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(),
+        # tf.keras.layers.Flatten(),
+        tf.keras.layers.Reshape((1,6)),
         tf.keras.layers.Dense(64, activation='relu'),
         tf.keras.layers.Dense(128, activation='relu'),
         tf.keras.layers.Dropout(0.7),
--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import tensorflowjs as tfjs
 
 from model import generate_model
 
@@ -10,33 +11,36 @@ def fetch_dataset():
     print(images.shape, labels.shape)
     images = tf.constant(images, dtype=tf.float32) # X is a np.array
     labels = tf.constant(labels, dtype=tf.float32) # y is a np.array
-    dataset = tf.data.Dataset.from_tensor_slices((images, labels))
+    dataset = tf.data.Dataset.from_tensor_slices(images)
 
-    return dataset
+    return images, labels
 
 
-def train(train_model, dataset):
+def train(train_model, x, y):
     lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=0.05, decay_steps=10000, decay_rate=0.9)
     optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
-    loss_fn = tf.keras.losses.MeanSquaredError()
-    # loss_fn = tf.keras.losses.Huber(delta=0.5, reduction="auto", name="huber_loss")
+    # loss_fn = tf.keras.losses.MeanSquaredError()
+    loss_fn = tf.keras.losses.Huber(delta=0.5, reduction="auto", name="huber_loss")
+    filepath = "./weights/v006/save_{epoch:02d}.h5"
+    checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath=filepath, monitor="loss", verbose=1, save_best_only=True, mode="min")
     train_model.compile(optimizer=optimizer,
                         loss=loss_fn,
-                        metrics=['accuracy'])
-    train_model.fit(dataset, batch_size=32, epochs=2000)
+                        metrics=['mse', 'accuracy'])
+    train_model.fit(x, y, batch_size=32, epochs=190, callbacks=[checkpoint])
     return train_model
 
 
 def save_model(train_model):
-    train_model.save('./weights/v001.h5')
+    train_model.save('./weights/v006.h5')
+    tfjs.converters.save_keras_model(train_model, './weights/v006_js')
 
 
 def main():
     train_model = generate_model()
     print("fetching")
-    dataset = fetch_dataset()
+    x, y = fetch_dataset()
     print("training")
-    train_model = train(train_model, dataset)
+    train_model = train(train_model, x, y)
     save_model(train_model)
 
 
